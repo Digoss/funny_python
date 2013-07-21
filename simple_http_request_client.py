89bc9ce82ac3d36fd8847821602ad7934b7b8361
diego bueno <die482@gmail.com>
2013-07-20 23:45:50 -0300

    Initial commit - client and server examples

---
+++ simple_http_request_client.py
@@ -0,0 +1,56 @@
+#Socket client
+
+import socket
+import sys
+import argparse
+import time
+
+def init_client(host, port):
+
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	except socket.error, msg:
+		print "Failed to create socket. Error code: "+str(msg[0])+" , Error message: "+str(msg[1])
+		sys.exit()
+
+	print "Socket Created"
+
+	try:
+		remote_ip = socket.gethostbyname(host)
+	except socket.gaierror:
+		print "Hostname could not be resolved. Exiting"
+		sys.exit()
+
+	print 'Ip address of '+host+' is '+remote_ip
+
+	s.connect((remote_ip,port))
+
+	print "Socket connected to "+host+" on ip "+remote_ip
+
+	message = "GET / HTTP/1.1\r\n\r\n"
+
+	try:
+		s.sendall(message)
+	except socket.error:
+		print "Send Failed"
+		sys.exit()
+	print "Message send successfully"
+
+	reply = s.recv(4096)
+	print reply
+
+	s.close()
+
+	print "Socket closed"
+
+def main():
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-p", "--port", help="specify the port to connect", type=int, required=True)
+	parser.add_argument("-H", "--host", help="specify the host to connect", required=True)
+	args = parser.parse_args()
+
+	init_client(args.host, args.port)
+
+if __name__ == "__main__":
+	main()
---
+++ simple_reply_server.py
@@ -0,0 +1,56 @@
+#Socket server
+
+import socket
+import sys
+import argparse
+from thread import *
+
+def init_server(host, port):
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	except socket.error, msg:
+		print "Failed to create socket. Error code: "+str(msg[0])+" , Error message: "+str(msg[1])
+		sys.exit()
+
+	try:
+		s.bind((host, port))
+	except socket.error, msg:
+		print "Bind failed. Error code: "+str(msg[0])+" Message"+msg[1]
+		sys.exit()
+
+	print "Socket bind complete"
+
+	s.listen(10)
+	print "Socket now listening"
+
+	while True:
+		try:
+			(conn, addr) = s.accept()
+			print "Connected with "+addr[0]+":"+str(addr[1])
+			start_new_thread(clienthandler, (conn,))
+		except KeyboardInterrupt:
+			s.close()
+			sys.exit()
+	s.close()
+
+def clienthandler(conn):
+	conn.send("Welcome to the server. Type something and hit enter\n")
+	while True:
+		data = conn.recv(1024)
+		reply = "OK..."+data
+		if not data:
+			break
+		conn.sendall(reply)
+	conn.close()
+
+def main():
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-p", "--port", help="specify the port to connect", type=int, required=True)
+	parser.add_argument("-H", "--host", help="specify the host to connect", default="")
+	args = parser.parse_args()
+
+	init_server(args.host, args.port)
+
+if __name__ == "__main__":
+	main()
